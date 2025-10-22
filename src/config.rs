c98087876ae77d5cce7688b409e1b4300a90f7d0
Real Limoges <b.real.limoges@gmail.com>
2025-10-21 19:32:23 -0700

    add shadow configs for later

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code, unused_variables)]
 use crate::error::AppError;
 use std::env;
 
@@ -20,4 +21,35 @@ impl Config {
 
 fn get_env_var(key: &str) -> Result<String, AppError> {
     env::var(key).map_err(|_| AppError::Config(format!("Missing environment variable {}", key)))
+}
+
+#[derive(Debug, Clone)]
+pub struct ShadowDbConfig {
+    pub database_user: String,
+    pub database_pass: String,
+    pub database_host: String,
+    pub database_port: String,
+}
+
+impl ShadowDbConfig {
+    pub fn from_env() -> Result<Self, AppError> {
+        Ok(Self {
+            database_user: get_env_var("DATABASE_USER")?,
+            database_pass: get_env_var("DATABASE_PASS")?,
+            database_host: get_env_var("DATABASE_HOST")?,
+            database_port: get_env_var("DATABASE_PORT")?,
+
+        })
+    }
+}
+#[derive(Debug, Clone)]
+pub struct ShadowRedisConfig {
+    pub redis_url: String,
+}
+impl ShadowRedisConfig {
+    pub fn from_env() -> Result<Self, AppError> {
+        Ok(Self {
+            redis_url: get_env_var("REDIS_URL")?,
+        })
+    }
 }
